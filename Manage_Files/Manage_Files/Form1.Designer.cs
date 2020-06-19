fba2d482741658fed67d6cb601d1e6faa13c828b
pxdalza <pxdalza@cibertec.edu.pe>
2020-06-18 20:53:42 -0500

    files

---
+++ Manage_Files/Manage_Files/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Manage_Files
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtxtMessage = new System.Windows.Forms.RichTextBox();
+            this.btn_save_file = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtxtMessage
+            // 
+            this.rtxtMessage.Location = new System.Drawing.Point(12, 23);
+            this.rtxtMessage.Name = "rtxtMessage";
+            this.rtxtMessage.Size = new System.Drawing.Size(499, 301);
+            this.rtxtMessage.TabIndex = 0;
+            this.rtxtMessage.Text = "";
+            // 
+            // btn_save_file
+            // 
+            this.btn_save_file.Location = new System.Drawing.Point(132, 340);
+            this.btn_save_file.Name = "btn_save_file";
+            this.btn_save_file.Size = new System.Drawing.Size(248, 45);
+            this.btn_save_file.TabIndex = 1;
+            this.btn_save_file.Text = "Guardar archivo";
+            this.btn_save_file.UseVisualStyleBackColor = true;
+            this.btn_save_file.Click += new System.EventHandler(this.btn_save_file_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 407);
+            this.Controls.Add(this.btn_save_file);
+            this.Controls.Add(this.rtxtMessage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtxtMessage;
+        private System.Windows.Forms.Button btn_save_file;
+    }
+}
+
---
+++ Manage_Files/Manage_Files/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//Libreria que nos permite manejar los archivos y directorios
+using System.IO;
+//obtener mac
+using System.Net.NetworkInformation;
+
+namespace Manage_Files
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //C:\\Users\\adminfoco\\Desktop\\Manage_files\\Manage_Files\\Manage_Files\\bin\\Debug
+
+            //Obtenego path de la carpeta donde me encuentro ubicado al momento
+            String path = Directory.GetCurrentDirectory();
+            //corto la cadena de caracteres en base el caracter (\)
+            var aux = path.Split('\\');
+            String pth = aux[0];
+
+            for (int i = 1; i < aux.Length-2; i++)
+            {
+                pth = pth + "\\" + aux[i];
+            }
+
+            pth = pth + "\\files";
+
+            //verifico si la carpeta existe
+            if (Directory.Exists(pth))
+            {
+                MessageBox.Show("Existe la carpeta files");
+                //leer archivo
+                String message = File.ReadAllText(pth + "\\bienvenida.txt");
+                MessageBox.Show(message);
+            }
+            else
+            {
+                //si no existe la carpeta se crea
+               var dirctory = Directory.CreateDirectory(pth);
+               //MessageBox.Show(dirctory.FullName);
+               //MessageBox.Show("Se creo la carpeta files");
+                //Creo archivo
+                TextWriter txt = new StreamWriter(pth+"\\bienvenida.txt");
+                txt.Write("Hola usuario, ingresa un mensaje.");
+                txt.Close();
+
+                //leer archivo
+                String message = File.ReadAllText(pth + "\\bienvenida.txt");
+                MessageBox.Show(message);
+
+
+            }
+
+        }
+
+        private void btn_save_file_Click(object sender, EventArgs e)
+        {
+            String path = Directory.GetCurrentDirectory();
+            //corto la cadena de caracteres en base el caracter (\)
+            var aux = path.Split('\\');
+            String pth = aux[0];
+
+            for (int i = 1; i < aux.Length - 2; i++)
+            {
+                pth = pth + "\\" + aux[i];
+            }
+
+            pth = pth + "\\files";
+
+            if (File.Exists(pth + "\\message.txt"))
+            {
+                var ux = new Guid();
+
+                TextWriter txt = new StreamWriter(pth + "\\message"+ ux + ".txt");
+                txt.Write(rtxtMessage.Text);
+                txt.Close();
+            }
+            else
+            {
+                TextWriter txt = new StreamWriter(pth + "\\message.txt");
+                txt.Write(rtxtMessage.Text);
+                txt.Close();
+            }
+
+            
+
+            String firstMacAddress = NetworkInterface
+            .GetAllNetworkInterfaces()
+            .Where(nic => nic.OperationalStatus == OperationalStatus.Up && nic.NetworkInterfaceType != NetworkInterfaceType.Loopback)
+            .Select(nic => nic.GetPhysicalAddress().ToString())
+            .FirstOrDefault();
+
+            TextWriter txt2 = new StreamWriter(pth + "\\mac.xyz");
+            txt2.Write(firstMacAddress);
+            txt2.Close();
+
+        }
+    }
+}
